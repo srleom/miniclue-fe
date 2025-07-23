f358e024f32109cd092951c06461893d22b28396
srleom <shengrui.leom@gmail.com>
2025-07-23 21:41:44 +0800

    feat: use Vercel rewrites as a reverse proxy for Posthog
    - Set PostHog API host to a specific path and defined UI host for better integration.
    - Added rewrites in Vercel configuration to route requests to PostHog assets, enhancing data handling.

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -7,7 +7,8 @@ const isProductionDomain =
 
 if (isProductionDomain) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/relay-xYmY/",
+    ui_host: "https://us.posthog.com",
     defaults: "2025-05-24",
     capture_exceptions: true,
     debug: process.env.NODE_ENV === "development",
